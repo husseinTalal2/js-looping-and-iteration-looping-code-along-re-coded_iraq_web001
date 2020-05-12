4064d4c4c9dcd362115c1408d5582fec4eb017fd
Hussein Talal <hossientalal2@gmail.com>
2020-05-12 22:14:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 // Code your solutions in this file
 function writeCards(names, event){
+  const arr = [""];
   for (let i = 0 ; i<names.length ;i++){
-      return `thank you, ${names[i]}, for the ${event} gift.` ;
+      arr.push(`thank you, ${names[i]}, for the ${event} gift.`)  ;
   }
+  return arr
 }
 
 const names = ["Ada", "Brendan", "Ali"];
 
-const card = writeCards(names, 'surprise');
+const cards = writeCards(names, 'surprise');
