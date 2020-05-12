4e6f9e858ad6b860f39e8715518fbec995efbf8e
Hussein Talal <hossientalal2@gmail.com>
2020-05-12 22:17:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solutions in this file
 function writeCards(names, event){
-  const arr = [""];
+  const arr = [];
   for (let i = 0 ; i<names.length ;i++){
       arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
 
