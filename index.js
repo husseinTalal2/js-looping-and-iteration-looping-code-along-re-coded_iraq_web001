9b603c677ceec79dd37426f46a940ce04157d944
Hussein Talal <hossientalal2@gmail.com>
2020-05-12 22:16:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function writeCards(names, event){
   const arr = [""];
   for (let i = 0 ; i<names.length ;i++){
       arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift.`);
-      
+
   }
   return arr;
 }
