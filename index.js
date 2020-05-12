7cc9a01e15069c04e40b1913959c4f121d07c88b
Hussein Talal <hossientalal2@gmail.com>
2020-05-12 22:07:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+function writeCards(names, event){
+  for (let i = 0 ; i<names.length ;i++){
+      console.log(`thank you, ${names[i]}, for the ${event} gift. `);
+  }
+}
+
+const names = ["Ada", "Brendan", "Ali"];
+const event = "birthday";
+writeCards(names, event);
